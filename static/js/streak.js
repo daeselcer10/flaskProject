0cf031bb5c60d703cb49200bb9e143254b043753
daeselcer10 <115955082+daeselcer10@users.noreply.github.com>
2023-02-26 18:50:26 -0600

    Got rid of commas in function

--- static/js/streak.js
+++ static/js/streak.js
@@ -1,4 +1,5 @@
 //Code to make a streak for days played in a row.
+// Code done by me, Dae. hooray!
 // Probably need to move some of this code to  window.onload function() {} - the one in
 // line 24 of play.html
 
@@ -20,8 +21,7 @@ function breakStreak (date1, date2) {
       date1.getUTCDate() > date2.getUTCDate() + 1;
 }
 
-if (streakLastGuess, streakToday){
-  if (breakStreak(streakLastGuess, streakToday)){
+if (breakStreak(streakLastGuess, streakToday)){
     streakCount = 0;
     localStorage.setItem("myStreak", streakCount); // do i need to do this?
     console.log("Bye bye, streak!")
@@ -29,7 +29,6 @@ if (streakLastGuess, streakToday){
   }
   else {
     console.log("The break streak function returned false; streak not broken.")
-  }
 }
 
 // function to continue streak if you are playing on the next day
@@ -39,7 +38,6 @@ function continueStreak (date1, date2) {
       date1.getUTCDate() === date2.getUTCDate() +1;
 }
 
-if (streakLastGuess, streakToday){
   if (continueStreak(streakLastGuess, streakToday)){
     streakCount ++;
     localStorage.setItem("myStreak", streakCount);
@@ -48,7 +46,6 @@ if (streakLastGuess, streakToday){
   }
   else {
     console.log("The continue streak function returned false; it's either time to break the streak, or it's the same day.")
-  }
 }
 //function to check if 2 dates are the same - in this case, you would do nothing - do I need this?
 function doNothingToStreak (date1, date2) {
