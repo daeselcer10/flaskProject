2af6d21060fd7e58b5584e2acefc1433902407e1
daeselcer10 <115955082+daeselcer10@users.noreply.github.com>
2023-02-27 19:11:44 -0600

    Updated with some more code ideas for making streak work

--- static/js/potential_new_streak_stuff.js
+++ static/js/potential_new_streak_stuff.js
@@ -1,50 +1,28 @@
+// idea: listen for form submit.
+// when submit, create new variable with submit time
+// only update this once per day
+// then everything else will work as i have it
 
-// Get today's date
-let today = new Date();
-// Get last guess date from localStorage or set it to yesterday if not found
-let lastGuessDate = new Date(localStorage.getItem("last_guess_date") || yesterday);
-// Get streak count from localStorage or set it to one if not found
-let streakCount = localStorage.getItem("streakCount") || 1;
-// Set a local storage item for myStreak - removed from here
 
-// Function to break streak if gap of > 1 day
-function breakStreak(date1, date2) {
-  // Convert dates to milliseconds and get difference
-  let diff = Math.abs(date1.getTime() - date2.getTime());
-  // Return true if difference is more than one day (86400000 milliseconds)
-  return diff > 86400000;
-}
-
-// Function to continue streak if you are playing on the next day hurray
-function continueStreak(date1, date2) {
-  // Convert dates to milliseconds and get difference
-  let diff = Math.abs(date1.getTime() - date2.getTime());
-  // Return true if difference is exactly one day (86400000 milliseconds)
-  return diff === 86400000;
-}
-
-window.onload = function () {
 
     // Check if last guess date is equal to today's date
-    if (lastGuessDate.toDateString() === today.toDateString()) {
+   // if (lastDayPlayed.toDateString() === today.toDateString()) {
       // Do nothing as user has already guessed today
-    }
-    // Check if last guess date is one day before today's date
-    else if (continueStreak(lastGuessDate, today)) {
-      // Increment streak count by one and update localStorage
-      streakCount++;
-      localStorage.setItem("myStreak", JSON.stringify(streakCount));
-      document.getElementById("streak").innerHTML = JSON.stringify(streakCount);
-    }
-    // Otherwise reset streak count to one and update localStorage
-    else {
-      streakCount = 1;
-      localStorage.setItem("myStreak", JSON.stringify(streakCount));
-      document.getElementById("streak").innerHTML = JSON.stringify(streakCount);
-    }
 
    // Update last guess date only if it is different from today's date
-   if (lastGuessDate.toDateString() !== today.toDateString()) {
-     localStorage.setItem("last_guess_date", today);
-   }
-};
+   //if (lastGuessDate.toDateString() !== today.toDateString()) {
+     //localStorage.setItem("last_guess_date", today);
+
+const formForStreak = document.querySelector('form');
+const todayStreak = new Date().toDateString();
+localStorage.setItem("todayStreak", todayStreak);
+
+formForStreak.addEventListener('submit', function(){
+    const streakTime = new Date().toDateString();
+    localStorage.setItem("streakLastGuessDate", streakTime)
+    if (todayStreak !== streakTime){
+        localStorage.setItem("todayStreak", streakTime)
+    } else {
+        console.log("Do not update; user has already guessed today")
+    }
+})
