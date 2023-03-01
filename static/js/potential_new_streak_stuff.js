b1e082ab098c7824a29730c669a8ff123210fc8c
daeselcer10 <115955082+daeselcer10@users.noreply.github.com>
2023-02-28 18:45:57 -0600

    Last test didn't work, updated, perhaps this is right? Moved variables around and fixed streakTime and todayStreak. Changes to rules and FAQ.

--- static/js/potential_new_streak_stuff.js
+++ static/js/potential_new_streak_stuff.js
@@ -10,9 +10,9 @@ localStorage.setItem("todayStreak", todayStreak.toUTCString());
 
 formForStreak.addEventListener('submit', function(){
     const streakTime = new Date();
-    localStorage.setItem("streakLastGuessDate", streakTime.toUTCString())
     if (todayStreak.toDateString() !== streakTime.toDateString()){
-        localStorage.setItem("todayStreak", streakTime.toUTCString())
+        localStorage.setItem("streakLastGuessDate", streakTime.toUTCString())
+        console.log("Updated streakLastGuessDate, user has not guessed today")
     } else {
         console.log("Do not update; user has already guessed today")
     }
