668a199dca124fffd8c5a446fd58d27e44241081
daeselcer10 <115955082+daeselcer10@users.noreply.github.com>
2023-03-05 19:04:11 -0600

    Changed to have only one formula, slightly changed event listener. Does this work?

--- static/js/potential_new_streak_stuff.js
+++ static/js/potential_new_streak_stuff.js
@@ -6,14 +6,25 @@
 
 const formForStreak = document.querySelector('form');
 const todayStreak = new Date();
-localStorage.setItem("todayStreak", todayStreak.toUTCString());
+
+// below is old stuff
+
+//formForStreak.addEventListener('submit', function(){
+   // const streakTime = new Date();
+    //if (todayStreak.toDateString() !== streakTime.toDateString()){
+      //  localStorage.setItem("streakLastGuessDate", todayStreak.toDateString());
+       // console.log("Updated streakLastGuessDate, user has not guessed today")
+    //} else {
+      //  console.log("Do not update; user has already guessed today")
+    //}
+//})
 
 formForStreak.addEventListener('submit', function(){
     const streakTime = new Date();
-    if (todayStreak.toDateString() !== streakTime.toDateString()){
+    const todayForStreak = new Date(todayStreak);
+    if (datesAreSameDay(streakTime, todayForStreak)){
         localStorage.setItem("streakLastGuessDate", streakTime.toUTCString())
-        console.log("Updated streakLastGuessDate, user has not guessed today")
-    } else {
-        console.log("Do not update; user has already guessed today")
+    } else if (!datesAreSameDay(streakTime,todayForStreak)){
+        console.log("Do something here")
     }
 })
