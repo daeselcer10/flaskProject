e6758f464d232b22045a7a6d61f0f16c2f1ee7e5
daeselcer10 <115955082+daeselcer10@users.noreply.github.com>
2023-03-13 00:34:22 -0500

    Fixing errors caused by moving everything into separate js file

--- static/js/potential_new_streak_stuff.js
+++ static/js/potential_new_streak_stuff.js
@@ -2,51 +2,14 @@
 
 
 const formForStreak = document.querySelector('form');
-const todayStreak = new Date();
+const todayStreak = new Date().toUTCString();
 
 formForStreak.addEventListener('submit', function(){
-    const streakTime = new Date();
+    const streakTime = new Date().toISOString;
     const todayForStreak = new Date(todayStreak);
     if (datesAreSameDay(streakTime, todayForStreak)){
-        localStorage.setItem("streakLastGuessDate", streakTime.toUTCString())
+        localStorage.setItem("streakLastGuessDate", streakTime.toISOString())
     } else if (!datesAreSameDay(streakTime,todayForStreak)){
         console.log("Do something here")
     }
 })
-
-const streakLastGuess = localStorage.getItem("streakLastGuessDate");
-// Get the streak count from localStorage or set it to one if not found
-let streakCount = localStorage.getItem("streakCount") || 0;
-
-//function to break streak if gap of  > 1 day
-function breakStreak (date1, date2) {
-  return date1.getUTCFullYear() > date2.getUTCFullYear() + 1 ||
-      date1.getUTCMonth() > date2.getUTCMonth() + 1 ||
-      date1.getUTCDate() > date2.getUTCDate() + 1;
-}
-
-
-// function to continue streak if you are playing on the next day hurray - not currently using
-function continueStreak (date1, date2) {
-  return date1.getUTCFullYear() === date2.getUTCFullYear() + 1 ||
-      date1.getUTCMonth () === date2.getUTCMonth() + 1 ||
-      date1.getUTCDate() === date2.getUTCDate() + 1;
-}
-if (streakLastGuess) {
-    const breakLastGuessTimeStreak = new Date(streakLastGuess);
-    const todayToday = new Date();
-    if (breakStreak(todayToday, breakLastGuessTimeStreak)) { // this works
-        streakCount = 0;
-        localStorage.setItem("myStreak", JSON.stringify(streakCount));
-        console.log("Bye bye, streak!");
-        document.getElementById("streak").innerHTML = JSON.stringify(streakCount);
-    } else { // problem is here?
-        localStorage.getItem("myStreak");
-        streakCount = +streakCount; // converts to number
-        streakCount++; // this is not incrementing past one
-        localStorage.setItem("myStreak", JSON.stringify(streakCount)); // restore as string
-        console.log("Hurray, let's add one to your streak!");
-        console.log(JSON.stringify(streakCount));
-        document.getElementById("streak").innerHTML = JSON.stringify(streakCount);
-    }
-}
---
+++ static/js/streak_stuff.js
@@ -0,0 +1,50 @@
+
+// Use const for variables that don't change
+const formForStreak = document.querySelector('form');
+const todayStreak = new Date();
+
+// Use camelCase for variable names
+const streakLastGuessDate = localStorage.getItem("streakLastGuessDate");
+
+// Use numbers instead of strings for streak count
+let streakCount = localStorage.getItem("myStreak") || 0;
+
+// Use one function to check if dates are consecutive or not
+function isConsecutive(date1, date2) {
+  // Get the year, month and day of each date
+  const year1 = date1.getUTCFullYear();
+  const month1 = date1.getUTCMonth();
+  const day1 = date1.getUTCDate();
+  const year2 = date2.getUTCFullYear();
+  const month2 = date2.getUTCMonth();
+  const day2 = date2.getUTCDate();
+
+  // Check if the dates are one day apart
+  return (
+    (year1 === year2 && month1 === month2 && day1 === day2 + 1) || // Same month and year
+    (year1 === year2 && month1 === month2 + 1 && day1 === 0 && day2 === new Date(year2, month2 + 1, 0).getDate()) || // Next month and same year
+    (year1 === year2 + 1 && month1 === 0 && month2 === new Date(year2 + 1).getMonth() - 11 && day1 === new Date(year3).getDate() - new Date(year3).getDay() - new Date(year3).getMonth()) // Next year
+    );
+}
+
+if (streakLastGuessDate) {
+    // Parse the last guess date from the local storage
+    const lastGuessDate = new Date(streakLastGuessDate);
+
+    if (isConsecutive(todayStreak, lastGuessDate)) {
+        // Increment the streak count by one
+        streakCount++;
+        console.log("Hurray, let's add one to your streak!");
+    } else {
+        // Reset the streak count to zero
+        streakCount = 0;
+        console.log("Bye bye, streak!");
+    }
+
+    // Update the local storage with the new values
+    localStorage.setItem("myStreak", streakCount);
+    localStorage.setItem("streakLastGuessDate", todayStreak);
+
+    // Update the HTML element with the current streak count
+    document.getElementById("streak").innerHTML = streakCount;
+}
