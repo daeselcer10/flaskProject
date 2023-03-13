f5770b0fdecbac15a8b25d036af0e502fc7c0b44
daeselcer10 <115955082+daeselcer10@users.noreply.github.com>
2023-03-12 23:00:15 -0500

    Moving all streakCount stuff to separate js file; simplifying code, still not working

--- static/js/potential_new_streak_stuff.js
+++ static/js/potential_new_streak_stuff.js
@@ -1,7 +1,4 @@
-// idea: listen for form submit.
-// when submit, create new variable with submit time
-// only update this once per day
-// then everything else will work as i have it
+// why does nothing work why lord why
 
 
 const formForStreak = document.querySelector('form');
@@ -16,3 +13,40 @@ formForStreak.addEventListener('submit', function(){
         console.log("Do something here")
     }
 })
+
+const streakLastGuess = localStorage.getItem("streakLastGuessDate");
+// Get the streak count from localStorage or set it to one if not found
+let streakCount = localStorage.getItem("streakCount") || 0;
+
+//function to break streak if gap of  > 1 day
+function breakStreak (date1, date2) {
+  return date1.getUTCFullYear() > date2.getUTCFullYear() + 1 ||
+      date1.getUTCMonth() > date2.getUTCMonth() + 1 ||
+      date1.getUTCDate() > date2.getUTCDate() + 1;
+}
+
+
+// function to continue streak if you are playing on the next day hurray - not currently using
+function continueStreak (date1, date2) {
+  return date1.getUTCFullYear() === date2.getUTCFullYear() + 1 ||
+      date1.getUTCMonth () === date2.getUTCMonth() + 1 ||
+      date1.getUTCDate() === date2.getUTCDate() + 1;
+}
+if (streakLastGuess) {
+    const breakLastGuessTimeStreak = new Date(streakLastGuess);
+    const todayToday = new Date();
+    if (breakStreak(todayToday, breakLastGuessTimeStreak)) { // this works
+        streakCount = 0;
+        localStorage.setItem("myStreak", JSON.stringify(streakCount));
+        console.log("Bye bye, streak!");
+        document.getElementById("streak").innerHTML = JSON.stringify(streakCount);
+    } else { // problem is here?
+        localStorage.getItem("myStreak");
+        streakCount = +streakCount; // converts to number
+        streakCount++; // this is not incrementing past one
+        localStorage.setItem("myStreak", JSON.stringify(streakCount)); // restore as string
+        console.log("Hurray, let's add one to your streak!");
+        console.log(JSON.stringify(streakCount));
+        document.getElementById("streak").innerHTML = JSON.stringify(streakCount);
+    }
+}
